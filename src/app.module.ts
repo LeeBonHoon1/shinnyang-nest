3639b1bc1bbd1cf75aaf448597534633478185de
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-12-17 18:33:38 +0900

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './entity/user-entity';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 @Module({
   imports: [
@@ -11,9 +14,9 @@ import { UserModel } from './entity/user-entity';
       type: 'postgres',
       host: '127.0.0.1',
       port: 5434,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'typeormstudy',
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [UserModel],
       synchronize: true,
     }),
